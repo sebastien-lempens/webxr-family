de0612366b0eaafb5c003d508b93a0d6df78279c
telkeatelecom <telkeatelecom@gmail.com>
2023-05-21 21:23:28 +0200

    Init

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,30 @@
 import { Canvas, useFrame } from "@react-three/fiber";
-import { OrbitControls, SpotLight, useGLTF, Clone, Environment, useTexture } from "@react-three/drei";
+import { OrbitControls, SpotLight, useGLTF, Clone, Environment, useTexture, CubeCamera } from "@react-three/drei";
 import { Physics, RigidBody, CuboidCollider, CapsuleCollider } from "@react-three/rapier";
 import { VRButton, XR, Controllers } from "@react-three/xr";
 import { Player, Switches, Frames, Pedestal, Walls } from "./components";
+import { RepeatWrapping, sRGBEncoding } from "three";
 const Scene = () => {
   const { nodes } = useGLTF("scene.gltf");
   const { buttons, columns, floor, table, frames, framesMask, framesMask2, framesPaint, roof, walls, wallsCollider } = nodes;
   const [base, normal, metallic, roughness] = useTexture([
-    "textures/texture_base.webp",
+    "textures/texture_base.jpg",
     "textures/texture_normal.webp",
     "textures/texture_metallic.webp",
     "textures/texture_roughness.webp",
   ]);
   base.flipY = normal.flipY = metallic.flipY = roughness.flipY = false;
+  //  base.repeat.y = -1
   const textureProps = {
     map: base,
     metalnessMap: metallic,
     roughnessMap: roughness,
     normalMap: normal,
-    normalScale:[-0.2,0.2]
   };
 
   return (
     <>
+      <CubeCamera>{texture => <Environment map={texture} files={'texture'} />}</CubeCamera>
       <Physics debug={false}>
         {console.log("%c SCENE RENDERED", "color:purple;font-weight:bold")}
         <Player />
@@ -33,7 +35,7 @@ const Scene = () => {
           </RigidBody>
           <Walls object={walls} textureProps={textureProps} />
           <RigidBody type='fixed'>
-            <Clone object={floor} inject={<meshStandardMaterial envMapIntensity={0.35} {...textureProps} />} />
+            <Clone object={floor} inject={<meshStandardMaterial {...textureProps} />} />
           </RigidBody>
           <Clone visible={true} object={roof} inject={<meshStandardMaterial {...textureProps} normalMap={null} />} />
           <Pedestal object={table} textureProps={textureProps} />
@@ -48,21 +50,24 @@ const Scene = () => {
           <Frames object={[frames, framesMask, framesMask2, framesPaint]} textureProps={textureProps} />
         </group>
       </Physics>
+
       <OrbitControls makeDefault />
-      <Environment frames={5} blur={0} preset="sunset"  background />
-      <ambientLight intensity={0.4} color={"#fff"} />
+
+      <hemisphereLight intensity={3}    />
+      {/* 
+      <pointLight intensity={1.2} position={[0,2,0]} /> */}
       <SpotLight
         visible={true}
-        color={"#fff"}
+        color={"white"}
         castShadow
         position={[-8, 30, -8]}
-        distance={50}
-        angle={0.2}
-        anglePower={8}
+        distance={60}
+        angle={0.1}
+        anglePower={4}
         radiusTop={0}
-        radiusBottom={8}
-        attenuation={40}
-        intensity={0.4}
+        radiusBottom={7}
+        attenuation={50}
+        intensity={0.2}
       />
     </>
   );
